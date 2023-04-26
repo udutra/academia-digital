b103161833f3ada2a80cc9b305c4f86a632e9415
Guilherme Galarça Dutra <udutra@gmail.com>
2023-04-26 16:31:20 -0300

    Ajustes no projeto.

---
+++ src/main/java/me/dio/academia/academiadigital/controller/MatriculaController.java
@@ -0,0 +1,30 @@
+package me.dio.academia.academiadigital.controller;
+
+import jakarta.validation.Valid;
+import me.dio.academia.academiadigital.entity.Matricula;
+import me.dio.academia.academiadigital.entity.form.MatriculaForm;
+import me.dio.academia.academiadigital.service.impl.MatriculaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/matriculas")
+public class MatriculaController {
+
+    @Autowired
+    private MatriculaServiceImpl service;
+
+    @PostMapping
+    public Matricula create(@Valid @RequestBody MatriculaForm form) {
+        return service.create(form);
+    }
+
+    @GetMapping
+    public List<Matricula> getAll(@RequestParam(value = "bairro", required = false) String bairro) {
+        return service.getAll(bairro);
+    }
+
+}
---
+++ src/main/java/me/dio/academia/academiadigital/service/impl/MatriculaServiceImpl.java
@@ -0,0 +1,51 @@
+package me.dio.academia.academiadigital.service.impl;
+
+import me.dio.academia.academiadigital.entity.Aluno;
+import me.dio.academia.academiadigital.entity.Matricula;
+import me.dio.academia.academiadigital.entity.form.MatriculaForm;
+import me.dio.academia.academiadigital.repository.AlunoRepository;
+import me.dio.academia.academiadigital.repository.MatriculaRepository;
+import me.dio.academia.academiadigital.service.IMatriculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatriculaServiceImpl implements IMatriculaService {
+
+    @Autowired
+    private MatriculaRepository matriculaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public Matricula create(MatriculaForm form) {
+        Matricula matricula = new Matricula();
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        matricula.setAluno(aluno);
+
+        return matriculaRepository.save(matricula);
+    }
+
+    @Override
+    public Matricula get(Long id) {
+        return matriculaRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Matricula> getAll(String bairro) {
+
+        if(bairro == null){
+            return matriculaRepository.findAll();
+        }else{
+            return matriculaRepository.findAlunosMatriculadosBairro(bairro);
+        }
+
+    }
+
+    @Override
+    public void delete(Long id) {}
+}
